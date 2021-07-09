3efc3bdf71511e42bebb033d813d85315934b56d
Skydanenko Andrii <skidanenkoandrey@gmail.com>
2021-07-09 18:37:41 +0300

    work with variables, data types, and simple function alert, confirm, prompt

---
+++ Base_JavaScript/HelloWorld!.js
@@ -0,0 +1,31 @@
+// ПЕРЕМЕННЫЕ
+// Пример use strict'
+/*'use strict'
+let number
+number = 5;
+alert(number);*/
+
+//Работа с переменными
+/*let name = "John";
+let admin = name;
+alert(admin);*/
+
+//Наименования переменных
+/*let ourPlanetName = 'Terra';
+let currentUserName = 'John';*/
+
+//ТИПЫ ДАННЫХ
+/*
+number;
+string;
+bigint;
+undefined;
+boolean;
+symbol;
+null;
+object;*/
+
+// Взаимодействие: alert, prompt, confirm
+/*
+let yourName = prompt('Как тебя зовут?','');
+alert(`Your name is ${yourName}`);*/
